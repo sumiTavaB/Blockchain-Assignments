2cd1e8bbdcabaa16b5b98b59b6ec86b3f8993ef7
sumitava <sumitavabsws@gmail.com>
2022-08-07 01:37:51 +0530

    3rd level added

--- MerkleGeneration.java
+++ MerkleGeneration.java
@@ -1,9 +1,14 @@
 import java.io.*;
-import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MerkleGeneration {
+
+    static String[] hash_level_7 = new String[50];
+    static String[] hash_level_6 = new String[26];
+    static String[] hash_level_5 = new String[14];
+    static String[] hash_level_4 = new String[8];
+
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter the no. of blocks:");
@@ -19,10 +24,6 @@ public class MerkleGeneration {
             if (T % 2 == 1) {
                 transac[transac.length - 1] = transac[transac.length - 2];
             }
-            for (int j = 0; j < transac.length; j++) {
-                // System.out.println(" --> " + getMD5(transac[j]));
-                System.out.println(" --> " + transac[j]);
-            }
             String merkleRoot = generateMerkleRoot(transac, transac.length);
             System.out.println("MERKLE ROOT --> " + merkleRoot);
         }
@@ -30,14 +31,8 @@ public class MerkleGeneration {
 
     public static String generateMerkleRoot(String transaction[], int size)
             throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        String[] hash_level_7 = new String[50];
-        String[] hash_level_6 = new String[26];
-        String[] hash_level_5 = new String[14];
-        String[] hash_level_4 = new String[8];
-        String[] hash_level_3 = new String[4];
-        String[] hash_level_2 = new String[2];
-        String[] hash_level_1 = new String[1];
         String effective_case = "";
+        String merkleRoot = "";
         if (size > 26 && size <= 50) {
             effective_case = "A";
         } else if (size > 14 && size <= 26) {
@@ -51,41 +46,67 @@ public class MerkleGeneration {
         }
         switch (effective_case) {
             case "A":
-                int lb = 0, ub = 1, count = 0;
-                for (int i = 0; i < size; i++) {
-                    hash_level_7[i] = getMD5(transaction[i]);
-                }
-                for (int i = 0; i < size; i += 2) {
-                    if (hash_level_7[i].equals("") || hash_level_7.equals(null)) {
-                        break;
-                    } else {
-                        hash_level_6[i] = getMD5(hash_level_7[lb] + hash_level_7[ub]);
-                        lb += 2;
-                        ub += 2;
-                        ++count;
-                    }
-                }
+            case "D":
+                merkleRoot = eightLeafNode(transaction, size);
+                break;
             case "E":
-                lb = 0;
-                ub = 1;
-                for (int i = 0; i < size; i++) {
-                    hash_level_3[i] = getMD5(transaction[i]);
-                }
-                for (int i = 0; i < 2; i++) {
-                    hash_level_2[i] = getMD5(hash_level_3[lb] + hash_level_3[ub]);
-                    lb += 2;
-                    ub += 2;
-                }
-                hash_level_1[0] = getMD5(hash_level_2[0] + hash_level_2[1]);
+                merkleRoot = fourLeafNode(transaction, size, "TRANSACT");
+                break;
+            case default:
+                System.out.println("Unable to generate Merkle Root");
+                break;
         }
+        return merkleRoot;
+    }
+
+    public static String eightLeafNode(String transaction[], int size)
+            throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        String[] hash_level_3 = new String[4];
+        int lb = 0;
+        int ub = 1;
+        for (int i = 0; i < size; i++) {
+            hash_level_4[i] = getSHA256(transaction[i]);
+            System.out.println(transaction[i] + " --> " + hash_level_4[i]);
+        }
+        for (int i = 0; i < 4; i++) {
+            hash_level_3[i] = getSHA256(hash_level_4[lb] + hash_level_4[ub]);
+            System.out.println("(" + transaction[lb] + "+" + transaction[ub] + ") --> " + hash_level_3[i]);
+            lb += 2;
+            ub += 2;
+        }
+        return (fourLeafNode(hash_level_3, hash_level_3.length, "HASH"));
+    }
+
+    public static String fourLeafNode(String transaction[], int size, String type)
+            throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        String[] hash_level_3 = new String[4];
+        String[] hash_level_2 = new String[2];
+        String[] hash_level_1 = new String[1];
+        int lb = 0;
+        int ub = 1;
+        if (type.equals("TRANSACT")) {
+            for (int i = 0; i < size; i++) {
+                hash_level_3[i] = getSHA256(transaction[i]);
+            }
+        } else if (type.equals("HASH")) {
+            for (int i = 0; i < size; i++) {
+                hash_level_3[i] = transaction[i];
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            hash_level_2[i] = getSHA256(hash_level_3[lb] + hash_level_3[ub]);
+            lb += 2;
+            ub += 2;
+        }
+        hash_level_1[0] = getSHA256(hash_level_2[0] + hash_level_2[1]);
         return hash_level_1[0];
     }
 
-    public static String getMD5(String str)
+    public static String getSHA256(String str)
             throws NoSuchAlgorithmException, RuntimeException, UnsupportedEncodingException {
         byte[] bytesOfMessage = str.getBytes("UTF-8");
 
-        MessageDigest md = MessageDigest.getInstance("MD5");
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
         byte[] theMD5digest = md.digest(bytesOfMessage);
 
         final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
